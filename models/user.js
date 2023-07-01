1bfbf966f3f1eb1bd190feb65f2d0aa8694e3712
punisher435 <kotharichirag1989@gmail.com>
2023-07-01 22:45:33 +0530

    created auth

--- models/user.js
+++ models/user.js
@@ -16,6 +16,12 @@ const userSchema = new Schema({
         type: String,
         required:true,
     },
+    password: {
+        type: String,
+        required: [true, "Please Enter Your Password"],
+        
+        select: false,
+      },
     resume:
     {
         type: String,
